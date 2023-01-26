8002d67ec949b5794372465d554c6df6355fe800
Prakash Reddy <psomareddy@kj74dr47f2.mynetworksettings.com>
2023-01-25 23:07:54 -0500

    refactor sampleinst

--- src/main/java/com/splunk/field/extension/SampleInstrumentation.java
+++ src/main/java/com/splunk/field/extension/SampleInstrumentation.java
@@ -3,6 +3,7 @@ package com.splunk.field.extension;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
 
+import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.javaagent.bootstrap.Java8BytecodeBridge;
@@ -14,35 +15,36 @@ import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 
 public class SampleInstrumentation implements TypeInstrumentation {
-	
-	public SampleInstrumentation() {
-		
-	}
+
+	private static String originalClass = "com.simple.app.App";
+	private static String originalMethod = "getGreeting";
+	private static int originalMethodArgumentCount = 1;
 
 	@Override
 	public ElementMatcher<TypeDescription> typeMatcher() {
-		return AgentElementMatchers.hasSuperType(named("com.simple.app.App"));
+		return AgentElementMatchers.hasSuperType(named(originalClass));
 	}
 
 	@Override
 	public void transform(TypeTransformer transformer) {
-		transformer.applyAdviceToMethod(named("getGreeting").and(takesArguments(1)), 
-				 this.getClass().getName() + "$AddCustomSpanTagsAdvice");
+		transformer.applyAdviceToMethod(named(originalMethod).and(takesArguments(originalMethodArgumentCount)),
+				this.getClass().getName() + "$ExtensionAdvice");
 	}
-	
-	@SuppressWarnings("unused")
-	  public static class AddCustomSpanTagsAdvice {
-	    @Advice.OnMethodEnter(suppress = Throwable.class)
-	    public static void onEnter(
-	        @Advice.Argument(0) int i,
-	        @Advice.Local("otelContext") Context context,
-	        @Advice.Local("otelScope") Scope scope) {
-	    	
-	    	Java8BytecodeBridge.currentSpan().setAttribute("custom-tag-name", "custom-tag-value" + i);
-	    }
-	    
-	    
-	  }
 
+	public static class ExtensionAdvice {
+		@Advice.OnMethodEnter(suppress = Throwable.class)
+		public static void onEnter(@Advice.Argument(0) int i, @Advice.Local("otelContext") Context context,
+				@Advice.Local("otelScope") Scope scope) {
+
+			Span currentSpan = Java8BytecodeBridge.currentSpan();
+			// notice that the span tag value of i is read from method argument
+			currentSpan.setAttribute("iteration.value", i);
+			// although hard-coded here, you will probably read the email value from some field,
+			// argument or method return value on the original class
+			currentSpan.setAttribute("splunk.user.email", "prakash.reddy@splunk.com");
+
+		}
+
+	}
 
 }
